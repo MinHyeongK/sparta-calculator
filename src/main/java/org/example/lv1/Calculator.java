7307d35781d60efe2993b4aeac0b41068a7c4ea3
MinHyeongK <syjjmh1@naver.com>
2025-07-18 14:05:00 +0900

    lv1 오완료

--- src/main/java/org/example/lv1/Calculator.java
+++ src/main/java/org/example/lv1/Calculator.java
@@ -5,50 +5,51 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean isRunning = true;
 
-        while(isRunning){
+        while(true){
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
+        System.out.print("첫 번째 숫자(양의 정수)를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
+        if (num1 < 0){
+            System.out.println("양의 정수만 입력해주세요.");
+            continue;
+        }
+
+        System.out.print("두 번째 숫자(양의 정수)를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
+        if (num2 < 0){
+            System.out.println("양의 정수만 입력해주세요.");
+            continue;
+        }
 
         System.out.print("사칙연산(+-*/) 기호를 입력하세요: ");
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
         char operation = sc.next().charAt(0);
-
-        int result = 0;
-        float divResult = 0;
         switch (operation){
             case '+':
-                result = num1 + num2;
-                System.out.println("결과: " + result);
+                System.out.println("결과: " + (num1 + num2));
                 break;
             case '-':
-                result = num1 - num2;
-                System.out.println("결과: " + result);
+                System.out.println("결과: " + (num1 - num2));
                 break;
             case '*':
-                result = num1 * num2;
-                System.out.println("결과: " + result);
+                System.out.println("결과: " + (num1 * num2));
             case '/':
                 if(num2 == 0){
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
                 }
-                divResult = (float)num1 / num2;
-                System.out.println("결과: " + divResult);
+                System.out.println("결과: " + (float)num1/num2);
                 break;
             default:
                 System.out.println("사칙연산(+-*/) 중 하나를 입력해주세요.");
             }
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = sc.nextLine();
+            String exit = sc.next();
             if (exit.equals("exit")){
-                isRunning = false;
                 System.out.println("계산기 종료");
                 sc.close();
                 break;
