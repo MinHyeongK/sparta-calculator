192e96b3726763e785070771dbf2676ada8c1c86
MinHyeongK <syjjmh1@naver.com>
2025-07-18 11:34:51 +0900

    test9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("git init");
-	System.out.println("test");
-	System.out.println("test222");
-	System.out.println("test222");
-	System.out.println("test222");
-	System.out.println("test222");
-	System.out.println("test333");
-	System.out.println("test444");
+		System.out.println("test");
+		System.out.println("test222");
+		System.out.println("test222");
+		System.out.println("test222");
+		System.out.println("test222");
+		System.out.println("test333");
+		System.out.println("test999");
     }
 }
